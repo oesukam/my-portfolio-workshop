35d8cfa4d78155d59c1a0cdfd51880124f868504
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-05-31 19:40:19 +0200

    add github info

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 
 class Resume extends Component {
+  state = {
+    github: {}
+  };
   componentDidMount() {
-    console.log("Did mount");
+    this.fetchMyGitHubProfile();
   }
 
-  fetchMyGitHubProfile() {}
+  fetchMyGitHubProfile() {
+    fetch("https://api.github.com/users/oesukam")
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          github: res || {}
+        });
+      });
+  }
 
   render() {
+    const { github } = this.state;
     return (
       <div className="container">
         <div className="columns">
@@ -46,6 +58,31 @@ class Resume extends Component {
                 <li>
                   <span className="has-text-weight-bold">2011 - 2015:</span>{" "}
                   University
+                </li>
+              </ul>
+
+              <h3 className="title is-4">Github</h3>
+              <hr />
+              <ul>
+                <li>
+                  <span className="has-text-weight-bold">Username: </span>
+                  {github.login}
+                </li>
+                <li>
+                  <span className="has-text-weight-bold">Location: </span>
+                  {github.location}
+                </li>
+                <li>
+                  <span className="has-text-weight-bold">Followers: </span>
+                  {github.followers}
+                </li>
+                <li>
+                  <span className="has-text-weight-bold">Following: </span>
+                  {github.following}
+                </li>
+                <li>
+                  <span className="has-text-weight-bold">Repos: </span>
+                  {github.public_repos}
                 </li>
               </ul>
             </div>
